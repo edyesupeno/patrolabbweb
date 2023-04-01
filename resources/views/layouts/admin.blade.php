bd4ff289eec1b058341a56ce075e5cbf9e659854
edyesupeno <edye.suwarnoe19@gmail.com>
2023-04-01 13:20:33 +0700

    update dropdown menu login

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -71,16 +71,13 @@
                         <li class="maximize"><a class="text-dark" href="{{ url('#!') }}" onclick="javascript:toggleFullScreen()"><i data-feather="maximize"></i></a></li>
                         <li class="profile-nav onhover-dropdown p-0 me-0">
                             <div class="media profile-media"><img class="b-r-10" src="{{ URL::asset('/template/assets/images/dashboard/profile.jpg') }}" alt="">
-                                <div class="media-body"><span>Emay Walter</span>
-                                    <p class="mb-0 font-roboto">Admin <i class="middle fa fa-angle-down"></i></p>
+                                <div class="media-body"><span>Edy SW</span>
+                                    <p class="mb-0 font-roboto">Super Admin <i class="middle fa fa-angle-down"></i></p>
                                 </div>
                             </div>
                             <ul class="profile-dropdown onhover-show-div">
                                 <li><a href="{{ url('#') }}"><i data-feather="user"></i><span>Account
                                         </span></a></li>
-                                <li><a href="{{ url('#') }}"><i data-feather="mail"></i><span>Inbox</span></a>
-                                </li>
-                                <li><a href="{{ url('#') }}"><i data-feather="file-text"></i><span>Taskboard</span></a></li>
                                 <li><a href="{{ url('#') }}"><i data-feather="settings"></i><span>Settings</span></a></li>
                                 <li><a href="{{ url('#') }}"><i data-feather="log-in"> </i><span>Log
                                             in</span></a></li>
